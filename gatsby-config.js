88763dbfd91cf18556ba739509bcda8cc2e07c36
Lauren Tan <laurent@netflix.com>
2019-03-02 21:44:08 -0800

    Add google analytics ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-135472857-1`,
       },
     },
     `gatsby-plugin-feed`,
