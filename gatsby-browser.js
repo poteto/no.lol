729ee93874477352fe6bc523a5cac480d38823c2
Lauren Tan <laurent@netflix.com>
2019-03-16 23:02:19 -0700

    Reload when serviceworker updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,6 @@ import './static/fonts/firacode.css';
 
 // prism theme
 import './static/prism-custom.css';
+
+// reload when serviceworker updates - https://github.com/gatsbyjs/gatsby/issues/9087#issuecomment-459105021
+export const onServiceWorkerUpdateReady = () => window.location.reload(true);
