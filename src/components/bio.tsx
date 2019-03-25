cd1fd12d140166a31e21029edf4b5ff5dbce4430
Lauren Tan <laurent@netflix.com>
2019-03-24 23:03:34 -0700

    Up quality of small bio pic

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -8,7 +8,7 @@ const bioQuery = graphql`
   query BioQuery {
     avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 50, height: 50, quality: 80) {
           ...GatsbyImageSharpFixed
         }
       }
